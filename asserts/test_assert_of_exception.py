5ff02906dbcd0454de3abc4fe7f3962f4a50fccb
sometimes-na1ve <charlie47.dev@gmail.com>
2024-06-18 08:05:00 +0800

    [asserts] add assertion samples of exception

--- asserts/test_assert_of_exception.py
+++ asserts/test_assert_of_exception.py
@@ -1,3 +1,5 @@
+import logging
+
 import pytest
 
 
@@ -7,12 +9,27 @@ def test_zero_division() -> None:
 
 
 def test_recursion() -> None:
-
     with pytest.raises(RuntimeError) as exception_wrapper:
-
         def foo() -> None:
             foo()
 
         foo()
 
     assert "maximum recursion" in str(exception_wrapper.value)
+
+
+def test_foo_not_implemented() -> None:
+    def foo() -> None:
+        raise NotImplementedError
+
+    with pytest.raises(RuntimeError) as exception_wrapper:
+        foo()
+
+    # you have to check exception type explicitly
+    # otherwise, pytest.raises(...) will only check iff a subclass
+    assert exception_wrapper.type is RuntimeError
+
+
+def test_message_match() -> None:
+    def foo() -> None:
+        raise ValueError("Exception 77 raised")
